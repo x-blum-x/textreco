8a45b67e3789380b2b82b082d2c3c7fc4479479a
x-blum-x <gabriel01102002@hotmail.com>
2024-06-04 16:54:04 -0400

    ajustes

--- API.py
+++ API.py
@@ -14,4 +14,4 @@ def extract_text():
     return text
 
 if __name__ == '__main__':
-    app.run(port=3000,debug=True)##mudar conforme necessidades de hospedagem
+    app.run(port=3000,debug=True)##mudar conforme as necessidades de hospedagem
